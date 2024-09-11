ec275f47d9841a1f2d06cb5217c43aada0af1feb
FrancescoSisti <francescosisti61@gmail.com>
2024-09-11 15:30:40 +0200

    added axios and compiled js

--- script/script.js
+++ script/script.js
@@ -1,28 +1,32 @@
 console.log(`JS OK`);
 
-// Creazione dell'app Vue
+// Creo l'app Vue
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            emails: []
+            emails: [],
+            loadingCompleted: false
         }
     },
     methods: {
-        generaEmail() {
-            for (let i = 0; i < 10; i++) {
-                fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then(response => response.json())
-                    .then(data => {
-                        this.emails.push(data.response);
-                    })
-                    .catch(error => console.error('Errore durante la richiesta:', error));
+        async generateEmails() {
+            try {
+                const requests = Array(10).fill().map(() =>
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                );
+                const responses = await Promise.all(requests);
+                this.emails = responses.map(response => response.data.response);
+                this.loadingCompleted = true;
+            } catch (error) {
+                console.error('Error while generating emails:', error);
             }
         }
     },
     mounted() {
-        this.generaEmail();
+        this.generateEmails();
     }
 }).mount('#app');
+
 
